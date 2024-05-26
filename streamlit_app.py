babc40a937de42be7e1aba34224a494ec163424a
kritimauludin <kritimauludin24@gmail.com>
2024-05-26 15:30:26 +0700

    update 26 may 2024 15.30

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit as st
 import numpy as np
 from math import *
 from tensorflow.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 url = "https://weather.com/id-ID/weather/hourbyhour/l/649d5fb3e40a54653934eae4ec15137dc83b1a1d6f5a4fc2accbb73a0a7e0e39"
 page = urlopen(url)
@@ -11,8 +14,33 @@ html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
 
+df=pd.read_csv("https://raw.githubusercontent.com/kritimauludin/prediction-speed-courir/main/dataset/user-distribution-all.csv", parse_dates=["created_at"],index_col=[0])
+df = df.drop(["distribution_code", "customer_code", "total", "process_at", "received_at", "courier_last_stamp", "status", "updated_at"], axis='columns')
+df = df.drop(["start_latitude", "start_longitude", "dest_latitude", "dest_longitude", "created_at"], axis='columns')
 
 model = load_model('ModelPredictionSpeedCourierKeras.h5')
+
+X = df.drop(columns='duration')
+y = df.duration
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)# Menentukan banyaknya data test yaitu sebesar 20% data
+
+
+scalerX = MinMaxScaler()
+scalerY = MinMaxScaler()
+
+X_train = scalerX.fit_transform(X_train)
+X_test = scalerX.transform(X_test)
+
+
+y1 = np.array(y_train)
+y2 = np.array(y_test)
+
+y_train = y1[:, np.newaxis]
+y_test = y2[:, np.newaxis]
+
+y_train = scalerY.fit_transform(y_train)
+y_test = scalerY.transform(y_test)
 
 courierSpeedPrediction = 0
 predictRain = soup.find('span', attrs={'data-testid':'PercentageValue'}).text
@@ -33,6 +61,22 @@ def haversine(long1, lat1, long2, lat2):
     r = 6371  # Radius in km use 6371. Use 3956 for miles
     return c * r
 
+def PredictionSpeedCourier(Distance, SpeedAverage, PotentialRain):
+    Distance = int(Distance)
+    SpeedAverage = int(SpeedAverage)
+    PotentialRain = int(PotentialRain)
+
+    x = np.array([[Distance, SpeedAverage, PotentialRain]])
+    x = scalerX.transform(x)
+
+
+    predictData = np.expand_dims(x, axis=1)
+
+    hasil = model.predict(predictData)
+
+    duration=scalerY.inverse_transform(hasil)
+
+    return duration
 
 #pembagian kolom
 col1, col2 = st.columns(2)
@@ -75,10 +119,10 @@ with col2 :
 with col1 : 
     if st.button("Prediksi Sekarang") :
         # Prepare the input data
-        input_data = np.array([[distance, speedAverage, potentialRain]])
-        input_data_reshaped = input_data.reshape((1, 1, 3))  # (samples, timesteps, features)
+        #input_data = np.array([[distance, speedAverage, potentialRain]])
+        #input_data_reshaped = input_data.reshape((1, 1, 3))  # (samples, timesteps, features)
 
-        courierSpeedPrediction = model.predict(input_data_reshaped)
+        courierSpeedPrediction = PredictionSpeedCourier(distance, speedAverage, potentialRain)
 with col2 :
     st.text("")
     st.text("")
